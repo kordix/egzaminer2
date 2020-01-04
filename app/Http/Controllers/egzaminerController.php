0d3e0bf434d23f7460c60eec532565b4ff83a6a6
= <=>
2020-01-04 16:29:54 +0100

    add

--- app/Http/Controllers/egzaminerController.php
+++ app/Http/Controllers/egzaminerController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Question;
 use App\Result;
-
+use App\Setting;
 use App\Tag;
 use App\Tagpivot;
 use Illuminate\Support\Facades\DB;
@@ -13,97 +13,110 @@ use Illuminate\Support\Facades\DB;
 
 class egzaminerController extends Controller
 {
-     public function questionsToTag($id){
-        $tags2=[$id];
+    public function questionsToTag($id)
+    {
+        $tags2 = [$id];
         // $rows = Tag::all();
         $rows = Question::with('tags')->whereHas('tags', function ($q) use ($tags2) {
             $q->whereIn('id', $tags2);
-        })->where('language', '=', 'DE')->join('results','questions.id','=','results.question_id')->get();
+        })->where('language', '=', 'DE')->join('results', 'questions.id', '=', 'results.question_id')->get();
         return $rows;
         // dd($rows);
         // return view('layouts.list', compact('rows', 'currentlanguage', 'categories', 'tags'));
     }
 
-    public function tagsToQuestion($id){
-        $rows = DB::table('questions_tags')->where('question_id','=',$id)->join('tags','questions_tags.tag_id','=','tags.id')->get();
+    public function tagsToQuestion($id)
+    {
+        $rows = DB::table('questions_tags')->where('question_id', '=', $id)->join('tags', 'questions_tags.tag_id', '=', 'tags.id')->get();
         // $rows = Tagpivot::where('question_id','=',$id)->join('questions');
         return $rows;
     }
 
-    public function update($id, Request $request){
+    public function update($id, Request $request)
+    {
         $question = Question::find($id);
 
         $question->update([
-            'question'=>request('question'),
-            'answer'=>request('answer')
+            'question' => request('question'),
+            'answer' => request('answer')
         ]);
     }
 
-    public function update2($id,Request $request){
-            $question = Result::find($id);
+    public function update2($id, Request $request)
+    {
+        $question = Result::find($id);
         // dd($request->counter);
-        $question->update(['counter'=>$request->counter]);
-
+        $question->update(['counter' => $request->counter]);
     }
 
-    public function update3($id, Request $request){
+    public function update3($id, Request $request)
+    {
         $question = Question::find($id);
         $question->update($request->all());
         dd($request->all());
-
     }
 
-    public function add(Request $request){
+    public function add(Request $request)
+    {
+        $language = Setting::find(1)->activelanguage;
+
+
+
         $row = Question::create([
             'answer' => $request->answer,
             'question' => $request->question,
             'rodzajnik' => $request->rodzajnik,
-            'language' => 'SP'
+            'language' => $language
 
         ]);
 
         Result::create([
-            'question_id'=>$row->id,
-            'counter'=>0,
-            'user_id'=>1
+            'question_id' => $row->id,
+            'counter' => 0,
+            'user_id' => 1
         ]);
         Tagpivot::create([
-            'question_id'=>$row->id,
-            'tag_id'=>$request->tag_id
+            'question_id' => $row->id,
+            'tag_id' => $request->tag_id
         ]);
 
+        dd($language);
     }
 
-    public function addTag(Request $request){
+    public function addTag(Request $request)
+    {
         Tag::create($request->all());
     }
 
-    public function deleteTag($id){
+    public function deleteTag($id)
+    {
         Tag::find($id)->delete();
     }
 
-    public function deleteTagToQuestion($qid,$tid){
-        $row = Tagpivot::where('question_id','=',$qid)->where('tag_id','=',$tid)->delete();
+    public function deleteTagToQuestion($qid, $tid)
+    {
+        $row = Tagpivot::where('question_id', '=', $qid)->where('tag_id', '=', $tid)->delete();
         // dd($row);
         // ->delete();
     }
 
-    public function addTagToQuestion($qid,$tid){
+    public function addTagToQuestion($qid, $tid)
+    {
         Tagpivot::create([
-            'question_id'=>$qid,
-            'tag_id'=>$tid
+            'question_id' => $qid,
+            'tag_id' => $tid
         ]);
         // dd($row);
         // ->delete();
     }
 
-    public function delete($id){
+    public function delete($id)
+    {
         Question::find($id)->delete();
     }
 
-    public function getQuestion($id){
+    public function getQuestion($id)
+    {
         return Question::find($id);
     }
-
-
 }
