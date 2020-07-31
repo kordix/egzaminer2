fc1e332b894cd811d5747e5bb8f6cf0721c8693f
root <root@kordi.hekko.net.pl>
2020-07-31 17:22:18 +0100

    app.vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="row">
+
+<appinstance :key="key" :activeobszar="$store.state.activeobszar" :instanceid="1" v-if="render"></appinstance>
+<appinstance :key="key2" :activeobszar="$store.state.activeobszar2" :instanceid="2" v-if="render2"></appinstance>
+
+<!-- <appinstance :activeobszar="$store.state.activeobszar2"></appinstance> -->
+</div>
+
+
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            key:0,
+            key2:1,
+            render:true,
+            render2:true
+        }
+    },
+    methods:{
+        rerender(){
+            this.render=false;
+            this.render=true;
+        }
+    }
+
+}
+</script>
+
+<style lang="css">
+</style>
+
